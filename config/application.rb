29b79b12d5ffec44f5a9d8c873a29aec5cd8db54
Alice D <alicedilanchian@gmail.com>
2021-02-08 15:16:58 -0500

    required dependencies

--- config/application.rb
+++ config/application.rb
@@ -21,22 +21,22 @@ Bundler.require(*Rails.groups)
 
 module IgBackend
   class Application < Rails::Application
-  config.generators do |g|
-    # Force new test files to be generated in the minitest-spec style
-    g.test_framework :minitest, spec: true
-  end
-  config.add_autoload_paths_to_load_path = true
-    # Initialize configuration defaults for originally generated Rails version.
-    # config.load_defaults 6.1
-    config.load_defaults 6.0
+    config.generators do |g|
+      # Force new test files to be generated in the minitest-spec style
+      g.test_framework :minitest, spec: true
+    end
+    config.add_autoload_paths_to_load_path = true
+      # Initialize configuration defaults for originally generated Rails version.
+      # config.load_defaults 6.1
+      config.load_defaults 6.0
 
-    Mongoid.load!('./config/mongoid.yml')
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+      Mongoid.load!('./config/mongoid.yml')
+      # Configuration for the application, engines, and railties goes here.
+      #
+      # These settings can be overridden in specific environments using the files
+      # in config/environments, which are processed later.
+      #
+      # config.time_zone = "Central Time (US & Canada)"
+      # config.eager_load_paths << Rails.root.join("extras")
   end
 end
