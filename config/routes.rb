3ec077f8e6e551d38aeb4e45582d4344efd915e3
Alice D <alicedilanchian@gmail.com>
2021-02-09 12:06:51 -0500

    route created for searchlandmark method, and route updated for searchlocation method after name changing from search to searchlocation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get '/photos/search/:country/:state/:city', to: 'photos#search', as: 'search'
+  get '/photos/searchlocation/:country/:state/:city', to: 'photos#searchlocation', as: 'searchlocation'
+  get '/photos/searchlandmark/:country/:state/:city', to: 'photos#searchlandmark', as: 'searchlandmark'
+
   resources :photos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #localhost:3000/photos?country=colombia&state=antioquia&city=guatape
