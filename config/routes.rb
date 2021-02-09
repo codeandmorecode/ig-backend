c2bca0ba3e67966c661fe4c3cab1ce17852a638b
Alice D <alicedilanchian@gmail.com>
2021-02-08 19:47:35 -0500

    route for search method working on postman

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/photos/search/:country/:state/:city', to: 'photos#search', as: 'search'
   resources :photos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #localhost:3000/photos?country=colombia&state=antioquia&city=guatape
+
 end
